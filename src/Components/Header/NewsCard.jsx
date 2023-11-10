59f818e576fc84cbf249bc07034dd622213150bc
Pragya Prabir Ranjan Deka <www.pprd@gmail.com>
2023-11-10 14:58:25 +0530

    UPD:Content

--- src/Components/Header/NewsCard.jsx
+++ src/Components/Header/NewsCard.jsx
@@ -6,9 +6,9 @@ import'./NewsCard.scss';
           <img src={article.urlToImage || 'https://via.placeholder.com/400x200'} alt="news-image" id="news-img" />
         </div>
         <div className="card-content">
-          <h3 id="news-title">{article.title || 'Title not available'}</h3>
+          <h3 id="news-title">{`${article.title}...click to read more` || 'Title not available'}</h3>
           <h6 className="news-source" id="news-source">{article.source ? `${article.source.name} · ${new Date(article.publishedAt).toLocaleString("en-US", { timeZone: "Asia/Jakarta" })}` : 'Source and Date not available'}</h6>
-          <p className="news-desc" id="news-desc">{article.description || 'Description not available'}</p>
+          <p className="news-desc" id="news-desc">{`${article.description}...click to READ MORE`|| 'Description not available'}</p>
         </div>
       </div>
     );
